2622c854783ea9552fe70badeccad5132425d455
Hamza Jabli <hamzajbali007@gmail.com>
2024-10-29 16:47:31 +0100

    Assignment

--- python/week 1/day 4/core/HTML-table/sever.py
+++ python/week 1/day 4/core/HTML-table/sever.py
@@ -1,1 +1,13 @@
-
+from flask import Flask, render_template 
+app =Flask(__name__)
+@app.route('/')
+def index():
+    users = [
+        {'first_name' : 'hamza', 'last_name' : 'jbali'},
+        {'first_name' : 'youssef', 'last_name' : 'jbali'},
+        {'first_name' : 'maram', 'last_name' : 'ayari'},
+        {'first_name' : 'wala', 'last_name' : 'nasri'}
+        ]
+    return render_template('index.html',users=users)
+if __name__=='__main__':
+    app.run(debug=True)
